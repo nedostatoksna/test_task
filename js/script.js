7fbddf5b7465c93f42340e58e9943e7563e6076f
Margarita Zhentsova <mzhentsova@gmail.com>
2024-09-14 00:46:28 +0400

    3 правки осталось

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const roomCardsGroup = document.querySelectorAll('.room-card');
+const roomTitles = document.querySelectorAll('.room-description__title');
+
+roomCardsGroup.forEach(card => {
+    const button = card.querySelector('.price-and-book__button');
+    
+    let isBooked = false; 
+
+    button.addEventListener('click', () => {
+        isBooked = true; 
+    });
+
+    card.addEventListener('mouseleave', () => {
+        if (isBooked) {
+            card.classList.add('booked'); 
+            isBooked = false; 
+        }
+    });
+});
+
+roomCardsGroup.forEach(card => {
+    card.addEventListener('click', (event) => {
+        const targetClassesToExclude = event.target.classList.contains('price-and-book__button') 
+            || event.target.classList.contains('room-booked') 
+            || event.target.classList.contains('room-booked__text')
+            || event.target.classList.contains('room-booked__text-wrapper')
+            || event.target.classList.contains('room-booked__link');
+            
+        if (targetClassesToExclude) {
+            return;  
+        }
+        else if (card.classList.contains('booked')) {
+            card.classList.remove('booked');
+        } 
+    });
+});
+
+roomTitles.forEach(title => {
+    title.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        this.classList.add('visited');
+    });
+});
